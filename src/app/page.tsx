f2d26f01f50a3a5a850b8cda354aab90ba647e1e
16_shrr <ctbaby1626@gmail.com>
2024-04-23 22:41:34 +0700

    Add an insert function to database and fetch data shown on the home page as list snippets.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+import { db } from "@/db";
 import Image from "next/image";
+import Link from "next/link";
+import { IoCodeSlash } from "react-icons/io5";
 
-export default function Home() {
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
   return (
-    <div>
-      Kontol Page
+    <div className="container max-w-7xl p-6 mx-auto min-h-screen flex flex-col justify-center items-center gap-10">
+      <div className="flex items-center justify-between w-full">
+        <h1 className="text-[32px] font-bold">List of Snippets</h1>
+        <Link href="/snippets/new" className="p-4 px-6 text-gray-50 bg-gradient-to-r from-blue-600 to-violet-600 rounded-full">
+          Create New Snippet
+          </Link>
+      </div>
+
+      <div className="w-full rounded-[12px] flex flex-col gap-4">
+        {snippets.map((code) => (
+          <div className="flex flex-col border-b-2 border-gray-300 pb-4" key={code.id}>
+            <div className="flex items-center justify-between">
+                <h3 className="font-semibold text-[20px] text-gray-700">
+                  {code.title}
+                </h3>
+
+              <div className="bg-gray-700 p-4 pt-12 rounded-[12px] flex-1 max-w-[800px] h-[150px] relative">
+                <div className="flex items-center gap-2 absolute top-0 left-0 translate-y-3 translate-x-4">
+                  <div className="w-4 h-4 bg-blue-600 rounded-full"/>
+                  <div className="w-4 h-4 bg-violet-600 rounded-full"/>
+                  <div className="w-4 h-4 bg-violet-700 rounded-full"/>
+                </div>
+                <IoCodeSlash className="absolute top-0 right-0 w-6 h-6 text-violet-600 translate-y-3 -translate-x-4"/>
+
+                <span className="text-gray-400">
+                  {code.code}
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,31 +1,54 @@
-import Image from 'next/image'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
 import React from 'react'
 import { IoCodeSlash } from 'react-icons/io5'
 import { MdSubtitles } from 'react-icons/md'
 
 const SnippetCreatePage = () => {
+    async function createSnippets(formData: FormData) {
+        // Need to be a server action
+        "use server"
+
+        //User input checks and validation
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
+
+        //Insert data to database
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        redirect('/')
+    }
+
     return (
         <div className='flex justify-center items-center min-h-screen container mx-auto px-12 w-full'>
-            <form className='bg-gray-100 py-10 px-8 md:w-[500px] rounded-[24px] shadow-[inset_2px_4px_8px_2px_#00000024]'>
+            <form action={createSnippets} className='bg-gray-100 py-10 px-8 md:w-[550px] rounded-[24px] shadow-lg'>
                 <div className='flex flex-col gap-10 items-center'>
-                    <h3 className='font-bold text-[32px] bg-gradient-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent '>
+                    <h3 className='font-bold text-[32px] bg-gradient-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent'>
                         Create a Snippet.
                     </h3>
 
                     <div className='flex flex-col items-center gap-4 w-full'>
-                        <div className='w-full flex border-2 border-gray-300 rounded-full items-center'>
+                        <div className='w-full flex border-2 border-violet-600 rounded-full items-center'>
                             <input
                                 name='title'
-                                placeholder='Title'
-                                className='px-4 py-3 rounded-full w-full bg-gray-100 focus:outline-none text-gray-400' />
-                            <MdSubtitles className='text-gray-400 w-7 h-7 mr-4' />
+                                placeholder='Defining types in TypeScript'
+                                className='px-4 py-3 rounded-full w-full bg-gray-100 focus:outline-none font-medium' required />
+                            <MdSubtitles className='text-blue-600 w-7 h-7 mr-4' />
                         </div>
                         <div className='relative w-full flex items-center overflow-hidden'>
                             <textarea
                                 name='code'
-                                placeholder='Code'
-                                className='px-4 py-3 w-full bg-gray-100 focus:outline-none text-gray-400 border-2 border-gray-300 rounded-[12px] h-[150px]' />
-                            <IoCodeSlash className='text-gray-400 w-6 h-6 mr-4 mb-2 absolute bottom-0 right-0' />
+                                placeholder='const user = {
+                                    name: "Hayes",
+                                    id: 0,
+                                  };'
+                                className='px-4 py-3 w-full bg-gray-100 focus:outline-none border-2 border-blue-600 rounded-[12px] h-[150px] font-medium' required />
+                            <IoCodeSlash className='text-violet-600 w-6 h-6 mr-4 mb-2 absolute bottom-0 right-0' />
                         </div>
 
                         <button type='submit' className='bg-gradient-to-r from-blue-600 to-violet-600 w-full p-4 rounded-full text-white'>
